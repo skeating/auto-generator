4703ef082f9b5116afd194212067b2608c5c9417
Frank Bergmann <fbergman@caltech.edu>
2014-06-30 17:10:16 +0200

    - whitespace warnings

--- generalFunctions.py
+++ generalFunctions.py
@@ -85,7 +85,7 @@ def parseAttribute(attrib):
       attTypeCode = 'ASTNode*'
     else:
       if attrib.has_key('element'):
-	    attTypeCode = '{0}*'.format(attrib['element'])
+        attTypeCode = '{0}*'.format(attrib['element'])
       else:
         attTypeCode = '{0}*'.format(strFunctions.cap(attrib['name']))
     num = False
@@ -156,7 +156,7 @@ def parseAttributeForC(attrib):
       attTypeCode = 'ASTNode_t*'
     else:
       #attTypeCode = 'element-not-done'
-	  attTypeCode = '{0}*'.format(strFunctions.cap(attrib['name']))
+      attTypeCode = '{0}*'.format(strFunctions.cap(attrib['name']))
     num = False
   elif attrib['type'] == 'lo_element':
     attType = 'lo_element'
