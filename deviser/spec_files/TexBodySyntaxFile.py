0f60bd7272950f6d9198feca7aeb1c9038da7fd9
Sarah Keating <skeating@caltech.edu>
2015-04-03 15:49:29 +0100

    finish the body section

--- deviser/spec_files/TexBodySyntaxFile.py
+++ deviser/spec_files/TexBodySyntaxFile.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python
 
+import re
+import os
+
 import BaseTexFile
 import BaseFile
 import strFunctions
@@ -22,6 +25,11 @@ class TexBodySyntaxFile(BaseTexFile.BaseTexFile):
 
         # derived members for description
         self.brief_description = 'Syntax section for specification'
+
+        # global member to keep track of classes written
+        self.classes_written = []
+
+        self.sort_attribute_names(self.plugins)
 
         BaseTexFile.BaseTexFile.__init__(self, 'body', 'tex',
                                          self.sbml_classes)
@@ -71,10 +79,76 @@ class TexBodySyntaxFile(BaseTexFile.BaseTexFile):
             self.write_attibute_paragraph(plugin['attribs'][i],
                                           extended_object)
 
+        # write body for child elements
+        for i in range(0, len(plugin['extension'])):
+            child = self.get_class(plugin['extension'][i]['name'])
+            self.write_body_for_class(child)
+
+        for i in range(0, len(plugin['lo_extension'])):
+            child = self.get_class(plugin['lo_extension'][i]['name'])
+            self.write_body_for_lo_class(child)
+
     ########################################################################
-    # Write rules for a class
+    # Write body for a list of class
+
+    def write_body_for_lo_class(self, sbml_class, nested=False):
+        lo_name = 'default'
+        if sbml_class['hasListOf']:
+            if 'lo_elementName' in sbml_class:
+                lo_name = strFunctions.upper_first(
+                    sbml_class['lo_elementName'])
+            else:
+                lo_name = strFunctions.cap_list_of_name(sbml_class['name'])
+
+        # do not write if already written
+        if lo_name in self.classes_written:
+            return
+        self.classes_written.append(lo_name)
+
+        if 'texname' in sbml_class:
+            classname = sbml_class['texname']
+        else:
+            classname = sbml_class['name']
+
+        self.write_comment_line('---------------------------------------------'
+                                '------------')
+        self.write_line('\subsection{0}The \class{0}{1}{2} class{2}'
+                        .format(self.start_b, lo_name, self.end_b))
+        self.write_line('\label{}{}-class{}'
+                        .format(self.start_b, lo_name.lower(), self.end_b))
+        self.skip_line()
+        self.write_to_do('explain {}'.format(lo_name))
+
+        self.write_line('The \\{0} object derives from the \\class{1}ListOf{2}'
+                        ' and inherits the core attributes and subobjects '
+                        'from the \\class{1}SBase{2} class.  It contains '
+                        'one or more objects of type \\{3}.'
+                        .format(lo_name, self.start_b, self.end_b, classname))
+        self.skip_line()
+
+        written = False
+        for i in range(0, len(sbml_class['lo_attribs'])):
+            if not written:
+                self.write_line('In addition the  \\{} object has the '
+                                'following attributes.'
+                                .format(lo_name))
+                self.skip_line()
+                written = True
+            att = sbml_class['lo_attribs'][i]
+            self.write_attibute_paragraph(att, lo_name)
+
+        if not nested:
+            self.write_body_for_class(self.get_class(sbml_class['name']))
+
+    ########################################################################
+    # Write body for a class
 
     def write_body_for_class(self, sbml_class):
+        if sbml_class['hasListOf']:
+            self.write_body_for_lo_class(sbml_class, True)
+        if sbml_class['name'] in self.classes_written:
+            return
+        self.classes_written.append(sbml_class['name'])
         base_class = self.look_up_base_class(sbml_class['baseClass'])
         if 'texname' in sbml_class:
             classname = sbml_class['texname']
@@ -89,8 +163,7 @@ class TexBodySyntaxFile(BaseTexFile.BaseTexFile):
                         .format(self.start_b, classname.lower(),
                                 self.end_b))
         self.skip_line()
-        self.write_comment_line('TO DO: explain {}'.format(sbml_class['name']))
-        self.skip_line()
+        self.write_to_do('explain {}'.format(sbml_class['name']))
 
         if len(sbml_class['attribs']) == 0:
             self.write_line('The \\{} object derives from the '
@@ -105,25 +178,31 @@ class TexBodySyntaxFile(BaseTexFile.BaseTexFile):
                         'this class.'
                         .format(classname, base_class))
 
+        child_elements = []
+        child_attribs = []
         for i in range(0, len(sbml_class['attribs'])):
             att = sbml_class['attribs'][i]
-            self.write_child_element(att, classname)
+            att_type = att['type']
+            if att_type == 'element' and att['element'] != 'RelAbsVector':
+                child_elements.append(att)
+            elif att_type == 'lo_element' or att_type == 'inline_lo_element':
+                child_elements.append(att)
+            else:
+                child_attribs.append(att)
+
+        for i in range(0, len(child_elements)):
+            self.write_child_element(child_elements[i], classname)
 
         written = False
-        for i in range(0, len(sbml_class['attribs'])):
+        for i in range(0, len(child_attribs)):
             if not written:
                 self.write_line('In addition the  \\{} object has the '
                                 'following attributes.'
                                 .format(classname))
                 self.skip_line()
                 written = True
-            att = sbml_class['attribs'][i]
+            att = child_attribs[i]
             self.write_attibute_paragraph(att, classname)
-
-        for i in range(0, len(sbml_class['lo_attribs'])):
-            att = sbml_class['lo_attribs'][i]
-            self.write_to_do('deal with attribute {} on a listOf{} element'
-                             .format(att['name'], classname))
 
     # Write rules for an attribute
     def write_attibute_paragraph(self, attrib, name):
@@ -175,14 +254,32 @@ class TexBodySyntaxFile(BaseTexFile.BaseTexFile):
                                 else 'exactly one', child_name))
 
     ########################################################################
+    # write parts of file that can be copied from text
+
+    def get_text(self, filename):
+        file_in = open(os.path.dirname(__file__) + '\\' + filename, 'r')
+        intro = file_in.read()
+        file_in.close()
+        pkg_name = '\\' + self.fullname + 'Package'
+        intro = re.sub('PACKAGENAME', pkg_name, intro)
+        return intro
+
     # Write namespace section
     def write_namespace_section(self):
         self.write_line('\\subsection{Namespace URI and other declarations '
                         'necessary for using this package}')
         self.write_line('\\label{xml-namespace}')
         self.skip_line()
-
-        self.write_to_do('Paragraph needed')
+        self.write_line(self.get_text('body_ns_section.txt'))
+        self.skip_line()
+        self.write_line('\\begin{center}')
+        self.write_line('\\uri{}http://www.sbml.org/sbml/level3/'
+                        'version1/{}/version1{}'
+                        .format(self.start_b, self.package, self.end_b))
+        self.write_line('\\end{center}')
+        self.skip_line()
+        self.write_line(self.get_text('body_ns_section2.txt'))
+        self.skip_line()
 
     #########################################################################
     # Write data types section
@@ -245,11 +342,11 @@ class TexBodySyntaxFile(BaseTexFile.BaseTexFile):
                                         enum['values'][0]['value'],
                                         self.end_b)
         for i in range(1, num_values-1):
-            enum_value = ', \\const{}{}{}'.format(self.start_b,
-                                                  enum['values'][i]['value'],
-                                                  self.end_b)
+            enum_value = ',\\const{}{}{}'.format(self.start_b,
+                                                 enum['values'][i]['value'],
+                                                 self.end_b)
             listed += enum_value
-        listed += ' and \\const{}{}{}'\
+        listed += ' and\\const{}{}{}'\
             .format(self.start_b, enum['values'][num_values-1]['value'],
                     self.end_b)
         return listed
@@ -265,14 +362,26 @@ class TexBodySyntaxFile(BaseTexFile.BaseTexFile):
                     return item['texname']
                 else:
                     return item['name']
-        return 'SBase' # default
+        return 'SBase'  # default
 
-    # function to determine if teh element belongs to this package
+    # function to determine if the element belongs to this package
     def derives_from_other_ns(self, name):
         for item in self.sbml_classes:
             if item['name'] == name:
                 return False
         return True
+
+    # function to get the class by its name
+    def get_class(self, name):
+        for i in range(0, len(self.sbml_classes)):
+            this_class = self.sbml_classes[i]
+            if this_class['name'] == name:
+                return this_class
+            elif this_class['hasListOf'] is True:
+                if strFunctions.cap_list_of_name(this_class['name']) == name:
+                    return this_class
+        return None
+
     #######################################################################
     # Write file
 
@@ -294,7 +403,6 @@ class TexBodySyntaxFile(BaseTexFile.BaseTexFile):
 
         self.write_namespace_section()
         self.write_primitive_data_types()
-        self.sort_attribute_names(self.plugins)
         for i in range(0, len(self.plugins)):
             self.write_body_for_extended_class(self.plugins[i])
         for i in range(0, len(self.sbml_classes)):
